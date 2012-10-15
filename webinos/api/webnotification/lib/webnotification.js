4ebdab1475d394bf558745b1ccf04a40ceba4ca3
André Paul <andre.paul@fokus.fraunhofer.de>
2012-10-15 16:34:04 +0200

    W3C WebNotification API
    Native Notification using notify-send (on windows also notify-send
    implementation must be available)
    
    Jira-issue: tbd

---
+++ webinos/api/webnotification/lib/webnotification.js
@@ -0,0 +1,73 @@
+/*******************************************************************************
+*  Code contributed to the webinos project
+* 
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*  
+*     http://www.apache.org/licenses/LICENSE-2.0
+*  
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+* 
+* Copyright 2012 André Paul, Fraunhofer FOKUS
+******************************************************************************/
+(function() {
+	var exec = require('child_process').exec;
+
+/**
+ * ...
+ * 
+ * ...
+ * @constructor
+ * @param rpcHandler A handler for functions that use RPC to deliver their result.
+ * @param params Parameters to initialize the service.
+ */
+var WebNotificationModule = function(rpcHandler, params) {
+	// inherit from RPCWebinosService
+	this.base = RPCWebinosService;
+	this.base({
+		api:'http://webinos.org/api/webnotification',
+		displayName:'Web Notification',
+		description:'Web Notification API'
+	});
+	
+}
+
+WebNotificationModule.prototype = new RPCWebinosService;
+
+/**
+ * ...
+ * @param params Array with parameters.
+ * @param successCB Success callback.
+ * @param errorCB Error callback.
+ * @param objectRef RPC object reference.
+ */
+WebNotificationModule.prototype.notify = function(params, successCB, errorCB, objectRef){
+	console.log("notify was invoked");
+	console.log(params);
+	
+	var title = params[0];
+	var body = params[1].body;
+	var icon = params[1].iconUrl;
+	
+	exec("notify-send \"" + title + "\" \"" + body + "\" -i \"" + icon + "\"", function(error, stdout, stderr){
+			console.log("Result: " + error + " " + stdout + " " + stderr);
+
+			if (error && typeof errorCB === "function") {
+				errorCB();
+				return;
+			}
+
+			successCB();
+		});
+	
+}
+
+// export our object
+exports.Service = WebNotificationModule;
+
+})();
